2ff29d526fa3f02e73dabef39b59e08e489ec4da
Roey Kelner <roeykelner@gmail.com>
2023-02-08 09:52:07 +0200

    now?

--- 2022/day2/target/debug/deps/day2-bf4e485a2c935c4c.d
+++
@@ -1,5 +0,0 @@
-/home/roeyk/dev/advent_of_code/advent_of_code_2022/day2/target/debug/deps/day2-bf4e485a2c935c4c.rmeta: src/main.rs
-
-/home/roeyk/dev/advent_of_code/advent_of_code_2022/day2/target/debug/deps/day2-bf4e485a2c935c4c.d: src/main.rs
-
-src/main.rs:
--- 2022/day2/target/debug/deps/day2-c6ef21e5b6fc9f91.d
+++
@@ -1,5 +0,0 @@
-/home/roeyk/dev/advent_of_code/advent_of_code_2022/day2/target/debug/deps/day2-c6ef21e5b6fc9f91: src/main.rs
-
-/home/roeyk/dev/advent_of_code/advent_of_code_2022/day2/target/debug/deps/day2-c6ef21e5b6fc9f91.d: src/main.rs
-
-src/main.rs:
